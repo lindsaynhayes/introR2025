1094eaa1aa7ea856af814800c639f9e3f35f9edf
Lindsay Hayes <lindsaynhayeslab@gmail.com>
2025-03-04 18:38:39 -0600

    R02

---
+++ readings/data/import_trick.R
@@ -0,0 +1,23 @@
+# IMPORTING TRICK USING read_csv
+
+library(readr)
+library(taylor)
+
+# import some data
+data <- taylor_all_songs[1:314,c(1,3,4,5,12:28)]
+
+# extract the shortcut indication of the class of each column
+types <- as.character(as.col_spec(data))
+
+# modify fields that you want to change i.e. c (character) to f (factor)
+types <- "fDicddididdddddiilfff"
+
+#save both the df and the column specifications
+write_csv(data, file = "data/taylorswift.csv")
+saveRDS(types, file = "data/taylorswift_col_spec.rds")
+
+# read in the data and specify the col_types
+data_tidy <- read_csv(file = "data/taylorswift.csv",
+                      col_names = TRUE,
+                      col_types = readRDS("data/taylorswift_col_spec.rds"))
+
